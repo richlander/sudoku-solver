e22651b1b07f27342caa001e74b39274bfaa8569
Rich Lander <rlander@microsoft.com>
2022-01-15 10:50:54 -0800

    TrySolveBox implemented pre-testing

--- src/sudoku-solver/Solvers/PointingMultiplesCandidateSolver.cs
+++ src/sudoku-solver/Solvers/PointingMultiplesCandidateSolver.cs
@@ -8,36 +8,56 @@ public class PointingMultiplesCandidateSolver : ICandidateSolver
         throw new NotImplementedException();
     }
 
-    public bool TrySolveBox(int index, Puzzle puzzle, out Candidates? removalCandidates)
+    public bool TrySolveBox(int index, Puzzle puzzle, out Candidates? pointingCandidates)
     {
         Candidates candidates = puzzle.Candidates;
-        removalCandidates = new();
-        bool results = false;
+        pointingCandidates = new();
+        bool candidatesFound = false;
 
+        // Get baseline data about box
         ReadOnlySpan<int> boxPositions = Puzzle.GetPositionsForBox(index);
-
-        ReadOnlySpan<int> pointingCandidates;
         int[] horizontalNeighbors = Puzzle.GetBoxIndexesForHorizontalNeighbors(index);
         int[] verticalNeighbors = Puzzle.GetBoxIndexesForVerticalNeighbors(index);
+
+        // Used to pivot on which rows should be checked, later
         ReadOnlySpan<int> allRows = new int[] {0, 1, 2};
 
         // iterate over all three rows in the box, looking for candidates
         for (int i = 0; i < 3; i++)
         {
-            // 
-            HashSet<int> otherRowSet = new(6 * 9);
+            if (candidatesFound)
+            {
+                break;
+            }
+
+            // Need to first validate that the box row has unsolved cells
+            int rowStartingCell = i * 3;
+            if (puzzle[rowStartingCell] > 0 &&
+                puzzle[rowStartingCell + 1] > 0 &&
+                puzzle[rowStartingCell + 2] > 0)
+                {
+                    continue;
+                }
+
+            // HashSet is used for the same pattern as LINQ Except
+            // https://github.com/dotnet/runtime/blob/03d92024f9924d8b05a96e785da154f1d89d858f/src/libraries/System.Linq/src/System/Linq/Except.cs#L94
+            // 54 = 2 rows * 3 cells * 9 possible candidates
+            HashSet<int> otherRowSet = new(54);
             // for each row, iterate over the other two rows
             // they establish the baseline data with their candidates
-            // we want to find candidates that are not in the other two rows
+            // goal: find candidates in this row that are not in the other two rows
             foreach(int row in allRows.Except(i))
             {
+                // there are (virtually) three rows of positions; pick one per `row`
                 foreach(int position in boxPositions.Slice(row * 3, 3))
                 {
-                    if (!candidates.Contains(position))
+                    // position will only have candidates if that position is unsolved
+                    if (puzzle[position] > 0)
                     {
                         continue;
                     }
 
+                    // Add all candidates for that position to the HashSet
                     otherRowSet.AddRange(candidates[position]);
                 }
             }
@@ -46,14 +66,15 @@ public class PointingMultiplesCandidateSolver : ICandidateSolver
             // each of the three cells is considered separately
             foreach(int position in boxPositions.Slice(i * 3, 3))
             {
-                if (!candidates.Contains(position))
+                // Only looking at unsolved positions
+                if (puzzle[position] > 0)
                 {
                     continue;
                 }
 
                 // values unique within row
-                pointingCandidates = candidates[position].Except(otherRowSet);
-                if (pointingCandidates.Length == 0)
+                ReadOnlySpan<int> rowCandidates = candidates[position].Except(otherRowSet);
+                if (rowCandidates.Length == 0)
                 {
                     continue;
                 }
@@ -63,27 +84,28 @@ public class PointingMultiplesCandidateSolver : ICandidateSolver
                 {
                     ReadOnlySpan<int> neighborBoxPositions = Puzzle.GetPositionsForBox(neighbor);
 
-                    // get cells for same row
+                    // get cells in neighbor box for same row
                     foreach(int np in neighborBoxPositions.Slice(i * 3, 3))
                     {
-                        if (!candidates.Contains(np))
+                        // Only looking at unsolved cells
+                        if (puzzle[np] > 0)
                         {
                             continue;
                         }
 
                         ReadOnlySpan<int> neighborCandidates = candidates[np];
-                        ReadOnlySpan<int> matches = pointingCandidates.Intersect(neighborCandidates);
+                        ReadOnlySpan<int> matches = rowCandidates.Intersect(neighborCandidates);
                         if (matches.Length > 0)
                         {
-                            removalCandidates.AddCandidates(np, matches);
-                            results = true;
+                            pointingCandidates.AddCandidates(np, matches);
+                            candidatesFound = true;
                         }
                     }
                 }
             }
         }
 
-        return results;
+        return candidatesFound;
     }
 
     public bool TrySolveRows(int index, Puzzle puzzle, Box box, ReadOnlySpan<int> neighbors)
